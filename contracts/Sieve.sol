22df3fc524dc8faf4d5078830cbfec8aa85ee876
Mike Pumphrey <mike.pumphrey@consensys.net>
2018-01-09 15:28:23 -0800

    Cleaning up variables

--- contracts/Sieve.sol
+++ contracts/Sieve.sol
@@ -15,7 +15,7 @@ contract Sieve {
     uint[] memory isNotPrime = new uint[](a-1);
 
     // This will house the primes when done
-    uint[] memory q = new uint[](a-1);
+    uint[] memory primesplus = new uint[](a-1);
 
     // Counting the number of primes
     uint numPrimes = 0;
@@ -25,34 +25,33 @@ contract Sieve {
     
     // Total entries in the array
     // Should be a-1
-    uint c = allNums.length;
+    uint totalNums = allNums.length;
 
     // Start at first index
     j = 0;
 
     // The number we're sieving
-    uint i = allNums[j];
+    uint sieveNum= allNums[j];
 
-    // k is index of the values 2, 4, 6, 8, 10 to be removed given sieve i=2
+    // k is index of the values 2, 4, 6, 8, 10 to be removed given sieve sieveNum=2
     // k starts with 0
     uint k = j;
-
 
     // The main sieve loop
 
     // while the number we're sieving is less than 
     // the sqrt of the input number...
-    while (i <= sqrt_a) {
+    while (sieveNum <= sqrt_a) {
 
       // TODO: ADD CHECK FOR SQRT
       
         // k is index of the values to be removed given sieve
-        // k starts with 0 (but it skips to k+i)
+        // k starts with 0 (but it skips to k+sieveNum)
         k = j;
         // while we haven't gone past the end of the array
-        while (k + i < c) {    
+        while (k + sieveNum < totalNums) {    
           // Ex: 0 -> 2, 2 -> 4, etc
-          k = k + i;
+          k = k + sieveNum;
           // if sieve number is 2, eliminate 4, then 6,
           isNotPrime[k] = 1;
         }
@@ -60,34 +59,35 @@ contract Sieve {
     // Once we've made it to the end of the array
     // Move to the next number in the list (Ex. 2 -> 3)
     j = j+1;
-    i = allNums[j];
+    sieveNum = allNums[j];
 
-    // Now we can reevaluate the while (i <= b) condition
+    // Now we can reevaluate the while (sieveNum <= sqrt_a) condition
 
     }
 
-    // Now we have allNums =  (2,3,4,5,6,7...)
+    // Now we have allNums    = (2,3,4,5,6,7...)
     //             isNotPrime = (0,0,1,0,1,0...)
-    //Time to create an array (2,3,5,7...)
+    // Time to create an array  (2,3,5,7...,0,0)
     for (j=0; j < a-1; j++) {
       if (isNotPrime[j] == 0) {
-        q[numPrimes] = allNums[j];
+        primesplus[numPrimes] = allNums[j];
         numPrimes++;  
       }
     }
 
+    // Here is where we say how many elements the output array has
     plist = new uint[](numPrimes);
     
-    //DEBUG
+    // primesplus has trailing zeroes
+    // plist is just the list of primes
     for (j=0; j < numPrimes; j++) {
-      plist[j] = q[j];
+      plist[j] = primesplus[j];
     }
-
-
 
   }
 
-
+  // Square root function
+  // sqrt(5) => 2
   function sqrt(uint x) public pure returns (uint y) {
     uint z = (x + 1) / 2;
     y = x;
